78ec9e58e2d02da6785ad0238ae00aa02125c6d3
Eruerufusama <34217008+Eruerufusama@users.noreply.github.com>
2022-01-12 03:05:25 +0100

    Moved menu into seperate component

---
+++ src/lib/components/SubredditMenu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let subreddit;
+</script>
+
+<ul>
+    <li><a sveltekit:prefetch href={`/r/${subreddit}/hot`}>Hot</a></li>
+    <li><a sveltekit:prefetch href={`/r/${subreddit}/new`}>New</a></li>
+    <li><a sveltekit:prefetch href={`/r/${subreddit}/rising`}>Rising</a></li>
+    <li><a sveltekit:prefetch href={`/r/${subreddit}/controversial`}>Controversial</a></li>
+    <li><a sveltekit:prefetch href={`/r/${subreddit}/top`}>Top</a></li>
+</ul>
+
+<style lang="sass">
+    ul
+        display: flex
+        gap: 2em
+</style>
